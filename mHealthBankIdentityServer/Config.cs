40768ad32bc4a2321cd47cc73da61c33aa44fc0c
dennydenny96 <dennydenny96@yahoo.com>
2021-08-20 15:54:20 +0700

    initiate commit

---
+++ mHealthBankIdentityServer/Config.cs
@@ -0,0 +1,55 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+
+namespace mHealthBankIdentityServer
+{
+    public static class Config
+    {
+        public static IEnumerable<ApiScope> ApiScopes =>
+            new List<ApiScope>
+            {
+                new ApiScope("resource.WRITE"),
+                new ApiScope("resource.READ")
+            };
+
+        public static IEnumerable<ApiResource> ApiResources => new[]
+        {
+            new ApiResource("mhb_bca_API")
+            {
+                Scopes = new List<string> { "resource.WRITE", "resource.READ"},
+                ApiSecrets = new List<Secret> {new Secret("ScopeSecret".Sha256())},
+                UserClaims = new List<string> {"role"}
+            }
+        };
+
+        public static IEnumerable<Client> Clients =>
+            new List<Client>
+            {
+                new Client
+                {
+                    ClientId = Environment.GetEnvironmentVariable("BCA_HostedPayment_ClientKey"),
+                    ClientName = "Client Mhb-Bca Credentials",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret(Environment.GetEnvironmentVariable("BCA_HostedPayment_ClientSecret").Sha256())
+                    },
+                    AllowedScopes = { "resource.WRITE", "resource.READ" }
+                },
+
+                // Testing
+                new Client
+                {
+                    ClientId = Environment.GetEnvironmentVariable("Testing_ClientKey"),
+                    ClientName = "Client Testing Credentials",
+                    AllowedGrantTypes = GrantTypes.ClientCredentials,
+                    ClientSecrets =
+                    {
+                        new Secret(Environment.GetEnvironmentVariable("Testing_ClientSecret").Sha256())
+                    },
+                    AllowedScopes = { "resource.WRITE", "resource.READ" }
+                },
+            };
+    }
+}
---
+++ mHealthBankIdentityServer/Startup.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace mHealthBankIdentityServer
+{
+    public class Startup
+    {
+        public IWebHostEnvironment Environment { get; }
+
+        public Startup(IWebHostEnvironment environment)
+        {
+            Environment = environment;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            _ = services.AddIdentityServer()
+                .AddInMemoryApiResources(Config.ApiResources)
+                .AddInMemoryApiScopes(Config.ApiScopes)
+                .AddInMemoryClients(Config.Clients)
+                .AddDeveloperSigningCredential();        //This is for dev only scenarios when you don’t have a certificate to use.
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            if (Environment.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseIdentityServer();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
